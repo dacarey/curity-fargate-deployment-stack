5e0ef6b3a5dca90c2fafcd12ec76dbd8b1636df1
Dave Carey <dacarey@gmail.com>
2023-01-02 10:52:37 +0000

    Refactoring in preperation for splittiong admin and runtime services

--- curity_fargate_cluster_stack/curity_fargate_service.py
+++ curity_fargate_cluster_stack/curity_fargate_service.py
@@ -11,8 +11,18 @@ from aws_cdk import (
 
 class CurityFargateService:
     def __init__(self, construct, curity_cluster, admin):
-        curityImage = self.chooseDockerImage(construct, admin)
+        #
+        # Prepare the Container and associated ECS Task
+        # ====================================================================
+        curityImage = CurityFargateService.chooseDockerAdminImage(construct)
 
+        curity_admin_task_definition = self.createCurityAdminTask(
+            construct, curityImage
+        )
+
+        #
+        #  The Load Balancer needs a public dns zone
+        # ====================================================================
         hosted_zone = route53.HostedZone.from_hosted_zone_attributes(
             construct,
             "HostedZone",
@@ -20,10 +30,16 @@ class CurityFargateService:
             hosted_zone_id="Z06756123LAL436U5E34J",
         )
 
-        curity_admin_task_definition = self.createCurityAdminTask(
-            construct, curityImage
-        )
-
+        #
+        #  Create the Load Balancer and the BackEnd Service
+        #  Additional notes compared to the simple doc examples
+        #  - This auto-handles creating a certificate for use with https
+        #  - It auto handles DNS registration with the supplied 'domain-name'
+        #  - We configure service discovery ( via cloud map) for the 
+        #    container tasks in the backend.
+        #  - We enable the SSM execute-command option to allow debugging
+        #    into the containers
+        # ====================================================================
         self.curity_service = ecspattern.ApplicationLoadBalancedFargateService(
             construct,
             "CurityService",
@@ -37,11 +53,11 @@ class CurityFargateService:
             cluster=curity_cluster,
             enable_execute_command=True,
             service_name="curity-admin-service",
-            cloud_map_options=ecs.CloudMapOptions(
-                name='admin'
-            )
+            cloud_map_options=ecs.CloudMapOptions(name="admin"),
         )
 
+        #
+        # Setup the HealthCheck ping from the LB
         self.curity_service.target_group.configure_health_check(
             port="4465", protocol=lb.Protocol.HTTP
         )
@@ -56,20 +72,24 @@ class CurityFargateService:
         )
 
     #
-    #  Choose a Docker Image based on whether we are acting
-    #  as an admin node or a runtime node
+    #  Choose the Docker Admin Image
     # =========================================================================
-    def chooseDockerImage(self, construct, admin):
-        if admin:
-            curityAdminImage = ecr_assets.DockerImageAsset(
-                construct,
-                "CurityAdminImage",
-                directory="../curity-docker-provisioning/clustered-env",
-                file="Dockerfile.admin",
-            )
+    @staticmethod
+    def chooseDockerAdminImage(construct):
+        curityAdminImage = ecr_assets.DockerImageAsset(
+            construct,
+            "CurityAdminImage",
+            directory="../curity-docker-provisioning/clustered-env",
+            file="Dockerfile.admin",
+        )
 
-            return curityAdminImage
+        return curityAdminImage
 
+    #
+    #  Choose the Docker Runtime Image
+    # =========================================================================
+    @staticmethod
+    def chooseDockerRuntimeImage(construct):
         curityRuntimeImage = ecr_assets.DockerImageAsset(
             construct,
             "CurityRuntimeImage",
@@ -92,13 +112,41 @@ class CurityFargateService:
     #      https://aws.amazon.com/blogs/containers/new-using-amazon-ecs-exec-access-your-containers-fargate-ec2/
     #      https://towardsthecloud.com/amazon-ecs-execute-command-access-container
     # =========================================================================
-    def createCurityAdminTask(self, construct, curityImage):
+    @staticmethod
+    def createCurityAdminTask(construct, curityImage):
         curity_admin_task_definition = ecs.FargateTaskDefinition(
             construct,
             "curity-admin-task",
             cpu=512,
             memory_limit_mib=2048,
         )
+
+        container_port_mappings = [
+            ecs.PortMapping(container_port=8443, host_port=8443),
+            ecs.PortMapping(container_port=6749, host_port=6749),
+            ecs.PortMapping(container_port=4465, host_port=4465),
+            ecs.PortMapping(container_port=4466, host_port=4466),
+        ]
+
+        curity_admin_task_definition.add_container(
+            "curity-admin-container",
+            image=ecs.ContainerImage.from_docker_image_asset(curityImage),
+            port_mappings=container_port_mappings,
+            logging=ecs.AwsLogDriver(
+                stream_prefix="curityadmin",
+                mode=ecs.AwsLogDriverMode.NON_BLOCKING
+            ),
+        )
+
+        #
+        # This list of permissions is curated from these sources
+        # 1/ The first 7 permissions are essentially derived from
+        #   https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task_execution_IAM_role.html
+        #   1a/ N.B. Possibly I could optimise this first set to use the AWS
+        #       recommended AmazonECSTaskExecutionRolePolicy.  See
+        #       https://www.evernote.com/client/web?login=true#?b=533976b7-9966-468e-a6bd-c643b2eef72a&n=9241277e-3b27-48d1-addb-770bb444655c&
+        # 2/ The additional set of ssmmessages permissions are derived from
+        #    https://aws.amazon.com/blogs/containers/new-using-amazon-ecs-exec-access-your-containers-fargate-ec2/
 
         curity_admin_task_definition.add_to_task_role_policy(
             iam.PolicyStatement(
@@ -110,31 +158,6 @@ class CurityFargateService:
                     "logs:DescribeLogGroups",
                     "ssm:GetParameters",
                     "aps:RemoteWrite",
-                ],
-                resources=["*"],
-            )
-        )
-
-        container_port_mappings = [
-            ecs.PortMapping(container_port=8443, host_port=8443),
-            ecs.PortMapping(container_port=6749, host_port=6749),
-            ecs.PortMapping(container_port=4465, host_port=4465),
-            ecs.PortMapping(container_port=4466, host_port=4466),
-        ]
-
-        self.container = curity_admin_task_definition.add_container(
-            "curity-admin-container",
-            image=ecs.ContainerImage.from_docker_image_asset(curityImage),
-            port_mappings=container_port_mappings,
-            logging=ecs.AwsLogDriver(
-                stream_prefix="curityadmin",
-                mode=ecs.AwsLogDriverMode.NON_BLOCKING
-            ),
-        )
-
-        curity_admin_task_definition.add_to_task_role_policy(
-            iam.PolicyStatement(
-                actions=[
                     "ssmmessages:CreateControlChannel",
                     "ssmmessages:CreateDataChannel",
                     "ssmmessages:OpenControlChannel",
